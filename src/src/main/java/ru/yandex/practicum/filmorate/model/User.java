96022ff95a100a6d63afec9ead99755346c8ceff
ChakNikolay87 <ChakNikolay@Yandex.ru>
2024-11-17 09:50:29 +0200

    Add files via upload

---
+++ src/src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private int id;
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Login cannot be empty")
+    @Size(min = 3, message = "Login must be at least 3 characters long")
+    private String login;
+
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
+
+    @NotNull(message = "Birthday cannot be null")
+    @Past(message = "Birthday must be a past date")
+    private LocalDate birthday;
+}
---
+++ src/src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -0,0 +1,45 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.*;
+
+@Component
+public class InMemoryFilmStorage implements FilmStorage {
+    private final Map<Integer, Film> films = new HashMap<>();
+    private int idCounter = 1;
+
+    @Override
+    public Film addFilm(Film film) {
+        film.setId(idCounter++);
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @Override
+    public Film updateFilm(Film film) {
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @Override
+    public Optional<Film> getFilmById(int id) {
+        return Optional.ofNullable(films.get(id));
+    }
+
+    @Override
+    public List<Film> getAllFilms() {
+        return new ArrayList<>(films.values());
+    }
+
+    @Override
+    public void deleteFilm(int id) {
+        films.remove(id);
+    }
+
+    @Override
+    public boolean exists(int id) {
+        return !films.containsKey(id);
+    }
+}
