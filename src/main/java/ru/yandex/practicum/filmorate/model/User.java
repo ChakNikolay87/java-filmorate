06bc9d162430f3918e6c877f78ea18611ef56061
ChakNikolay87 <ChakNikolay@Yandex.ru>
2024-12-17 09:42:37 +0200

    Add files via upload

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,37 @@
+package ru.yandex.practicum.filmorate.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+public class User {
+    private Long id;
+    @Email
+    private String email;
+    private String login;
+    private String name;
+    private LocalDate birthday;
+    @JsonIgnore
+    private final Set<Long> friends = new HashSet<>();
+
+    public Map<String, Object> toMap() {
+        Map<String, Object> values = new HashMap<>();
+        values.put("user_id", id);
+        values.put("email", email);
+        values.put("login", login);
+        values.put("name", name);
+        values.put("birthday", birthday.toString());
+        return values;
+    }
+}
